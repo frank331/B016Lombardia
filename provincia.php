b7a090e88ae8cd013254d781c979cb64dabde41f
Francesco Amendola <amendolafr@gmail.com>
2022-05-30 17:10:09 +0200

    Added count of pages and limited navigation to max page

--- provincia.php
+++ provincia.php
@@ -4,16 +4,6 @@
 </head>
 <body>
 <h2>Componenti per provincia</h2>
-<form action="/provincia.php" method="GET">
-  <input type="hidden" name="provincia" id="provincia" value="<?php echo $_GET['provincia'] ?>">
-  <input type="hidden" id="limit" name="limit" value="<?php echo $_GET['limit'] ?>">
-  Pagina: <?php echo $_GET['page'] ?>&ensp;-&ensp;Vai a:  
-  <?php
-  if ($_GET['page']-1 >= 1)
-    echo '<input type="submit" name = "page" value="'.($_GET['page']-1).'">';
-    ?>
-  <input type="submit" name = "page" value="<?php echo $_GET['page']+1 ?>">
-</form>
 <?php
 require_once 'connection.php';
 $limit = $_GET['limit'];
@@ -22,13 +12,33 @@ $page = $_GET['page'];
 $page = $page == null ? 1 : $page;
 $lowerLimit = $page > 1 ? ($page - 1) * $limit : 1; 
 $upperLimit = $limit * $page;
-$query = 'select *
-from (
-	select row_number() OVER(ORDER BY c.pk_componente) AS rowNumber, c.* 
+$query = 'select count(*)
     from prova_lombardia.componente c
-    where c.res_prov = \'' . $_GET['provincia'] . '\'
-    ) t2
-    where t2.rowNumber BETWEEN '.$lowerLimit.' and '.$upperLimit.';';
+    where c.res_prov = \'' . $_GET['provincia'] . '\'';
+$result = mysqli_query($connect, $query);
+$row = mysqli_fetch_row($result);
+$numOfRecords = $row[0];
+$numOfPages = ceil($numOfRecords / $limit);
+?>
+<form action="/provincia.php" method="GET">
+  <input type="hidden" name="provincia" id="provincia" value="<?php echo $_GET['provincia'] ?>">
+  <input type="hidden" id="limit" name="limit" value="<?php echo $_GET['limit'] ?>">
+  Pagina: <?php echo $page.' di '.$numOfPages ?> &ensp;-&ensp;Vai a:  
+  <?php
+  if ($page-1 >= 1)
+    echo '<input type="submit" name = "page" value="'.($page-1).'">';
+  if ($page+1 <= $numOfPages)
+    echo '<input type="submit" name = "page" value="'.($page+1).'">';
+  ?>
+</form>
+<?php
+$query = 'select *
+    from (
+      select row_number() OVER(ORDER BY c.pk_componente) AS rowNumber, c.* 
+        from prova_lombardia.componente c
+        where c.res_prov = \'' . $_GET['provincia'] . '\'
+        ) t2
+        where t2.rowNumber BETWEEN '.$lowerLimit.' and '.$upperLimit.';';
 $result = mysqli_query($connect, $query);
 echo '<table style="border: 1px solid; border-collapse: collapse;">';
 while ($row = mysqli_fetch_array($result)){
